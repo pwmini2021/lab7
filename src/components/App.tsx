47f8390a48b487f893f3096d44cac8dd26f51e60
mkle <mkle@netcompany.com>
2021-11-20 15:30:46 +0100

    step 5

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,16 @@ import {Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import Error404Page from "./pages/Error404Page";
+import Layout from "./Layout";
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path={"/"} element={<HomePage/>}/>
-            <Route path={"about"} element={<AboutPage/>}/>
-            <Route path={"*"} element={<Error404Page/>}/>
+            <Route path={"/"} element={<Layout/>}>
+                <Route index element={<HomePage/>}/>
+                <Route path={"about"} element={<AboutPage/>}/>
+                <Route path={"*"} element={<Error404Page/>}/>
+            </Route>
         </Routes>
     );
 }
